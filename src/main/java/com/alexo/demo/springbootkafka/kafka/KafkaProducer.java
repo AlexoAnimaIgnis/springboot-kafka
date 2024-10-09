fa06371612c97d9d2507e8985263aafae87ee3e7
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-10-09 12:46:24 +0800

    added kafka producer

---
+++ src/main/java/com/alexo/demo/springbootkafka/kafka/KafkaProducer.java
@@ -0,0 +1,23 @@
+package com.alexo.demo.springbootkafka.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        LOGGER.info("Sending message: {}", message);
+
+        kafkaTemplate.send("javaguides", message);
+    }
+}
